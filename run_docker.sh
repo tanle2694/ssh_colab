4994fcced5c6a392211f782edd504de56e3092ce
tanlm <tanle2694@gmail.com>
2020-06-10 22:00:32 +0700

    detach docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 docker build . -t go_docker
-docker run --rm -p 7000:7000 -p 8000:8000 -p 8001:8001 -p 4443:4443 -p 10000-10050:10000-10050 -t go_docker
+docker run --rm -d -p 7000:7000 -p 8000:8000 -p 8001:8001 -p 4443:4443 -p 10000-10050:10000-10050 -t go_docker
